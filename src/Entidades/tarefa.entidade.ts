8bd0b795e8d86750e78de03f7d5681a1aafc90e1
FelipeRodriguesDeveloper <feliperodriguesdeveloper@hotmail.com>
2022-05-02 14:52:19 -0300

    Commit Inicial

---
+++ src/Entidades/tarefa.entidade.ts
@@ -0,0 +1,11 @@
+export class Tarefa
+{
+    constructor(
+        public id: number,
+        public titulo: string,
+        public concluida: boolean
+    ) 
+    {}
+
+    
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,95 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Tarefa } from 'src/Entidades/tarefa.entidade';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent 
+{
+  public modo = "lista";
+  public tarefas: Tarefa[] = [];
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder)
+  {
+    this.form = this.fb.group(
+       {   
+         titulo: ['', Validators.compose([  
+           Validators.minLength(3),
+           Validators.maxLength(60),
+           Validators.required
+          ])]
+       }
+    )
+
+    this.loadLocalStorage();
+  }
+
+  salvar()
+  {  
+     const titulo = this.form.controls['titulo'].value;
+     this.tarefas.push(new Tarefa(this.tarefas.length + 1, titulo, false));
+     this.salvarLocalStorage();
+     this.limparFormulario()
+     this.modo = "lista";
+  }
+ 
+  remover(tarefa: Tarefa)
+  {
+      const indice = this.tarefas.indexOf(tarefa);
+
+      if(indice != -1)
+      {
+        this.tarefas.splice(indice, 1);
+      }
+
+      this.salvarLocalStorage();
+  }
+
+  limparFormulario()
+  {
+    this.form.reset();
+  }
+
+  marcarComoConcluida(tarefa: Tarefa)
+  {
+     tarefa.concluida = true; 
+     this.salvarLocalStorage();
+  }
+
+  marcarComoNaoConcluida(tarefa: Tarefa)
+  {
+    tarefa.concluida = false;
+    this.salvarLocalStorage();
+  }
+
+  salvarLocalStorage()
+  {
+    const dados = JSON.stringify(this.tarefas);
+    localStorage.setItem("tarefas",dados);
+  }
+
+  loadLocalStorage()
+  {
+    const dados = localStorage.getItem("tarefas");
+    
+    if(dados)
+    {
+      this.tarefas = JSON.parse(dados);
+    }
+    else
+    {
+      this.tarefas = [];
+    }
+  }
+  
+  trocarModo(modo: string)
+  {
+    this.modo = modo;
+  }
 }
+
+
